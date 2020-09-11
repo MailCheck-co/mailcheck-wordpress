0dedf8f79ee186be395ac2b295d02ed5fdb33ebb
Eugene Bolikhov <mrjekson@gmail.com>
2020-09-11 18:55:51 +0300

    options page, acf validation

--- email_check.class.php
+++ email_check.class.php
@@ -1,12 +1,45 @@
 <?php
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 class emailCheck
 {
     protected $hash;
     public $trust_rate = 0.5;
 
-    function __constructor($hash, $trust_rate = 0.5){
-        $this->hash = $hash;
+    function __construct($hash = false, $trust_rate = 0.5){
+        if ($hash) {
+            $this->hash = $hash;
+        } else {
+            $this->hash = get_option('ec_hash');
+        }
         $this->trust_rate = $trust_rate;
+        $this->init_plugin();
+    }
+
+    function init_plugin(){
+        if (get_option('ec_enable_core') == 1){
+            //add_filter('acf/validate_value/type=email', 'my_acf_validate_value', 10, 4);
+        }
+        if (get_option('ec_enable_acf') == 1){
+            add_filter('acf/validate_value/type=email', array($this,'validate_acf'), 10, 4);
+        }
+        if (get_option('ec_enable_cf7') == 1){
+            //add_filter('acf/validate_value/type=email', 'my_acf_validate_value', 10, 4);
+        }
+    }
+
+    function validate_acf($valid, $value, $field, $input_name){
+        // Bail early if value is already invalid.
+        if( $valid !== true ) {
+            return $valid;
+        }
+
+        // Prevent value from saving if it contains the companies old name.
+        $result = $this->check($value);
+
+        if( !empty($value) && !$result['check'] ) {
+            return __( 'This email have very poor trust rate.' );
+        }
+        return $valid;
     }
 
     function check($email)
@@ -23,7 +56,7 @@ class emailCheck
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         //curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . $bearer));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "Authorization: Bearer " . $this->hash));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
         /*curl_setopt($ch, CURLOPT_VERBOSE, true);
@@ -31,13 +64,13 @@ class emailCheck
         curl_setopt($ch, CURLOPT_STDERR, $verbose);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);*/
         $curl_result = curl_exec($ch);
-        echo "<pre>----------";
+        /*echo "<pre>----------";
         print_r($curl_result);
-        echo "</pre>";
+        echo "</pre>";*/
 
         if (!curl_errno($ch)) {
-            $info = curl_getinfo($ch);
-            echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'];
+            //$info = curl_getinfo($ch);
+            //echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'];
         } else {
             echo 'Curl error: ' . curl_error($ch);
             $result['message'] = curl_error($ch);
@@ -50,6 +83,9 @@ class emailCheck
             if ($curl_result->trustRate >= $this->trust_rate){
                 $result['check'] = true;
             }
+        } else {
+            // truest rate = 0
+            $result['check'] = false;
         }
         if (!empty($curl_result->message)){
             $result['message'] = $curl_result->message;
---
+++ email_check.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: Email Check
+Description: Check Email Trust Rate
+Version: 1.0
+Author: Eugene Bolikhov
+*/
+
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+function ec_add_plugin_page_settings_link( $links ) {
+    $links['settings'] = '<a href="' .
+        admin_url( 'options-general.php?page=ec-settings' ) .
+        '">' . __('Settings') . '</a>';
+    return $links;
+}
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'ec_add_plugin_page_settings_link');
+
+if ( is_admin() ) {
+    add_action('admin_menu', 'ec_plugin_menu');
+    add_action('admin_init', 'ec_register_settings' );
+}
+function ec_plugin_menu() {
+    add_options_page('EC Settings', 'Email Check', 'manage_options', 'ec-settings', 'ec_plugin_page');
+}
+
+function ec_register_settings() { // whitelist options
+    register_setting( 'ec-option-group', 'ec_hash' );
+    register_setting( 'ec-option-group', 'ec_enable_core' );
+    register_setting( 'ec-option-group', 'ec_enable_acf' );
+    register_setting( 'ec-option-group', 'ec_enable_cf7' );
+}
+
+function ec_plugin_page(){
+    require_once('options_page.php');
+}
+
+
+
+require_once "email_check.class.php";
+$emailCheck = new emailCheck();
+
